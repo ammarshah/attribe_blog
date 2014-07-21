e5fa426bfdebdb4ab50761616fe820612815304b
ammarshah <ammarshah123@gmail.com>
2014-07-21 17:28:21 +0500

    blog done with user auth

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,60 @@
+class ArticlesController < ApplicationController
+
+  before_filter :authorize
+
+  def authorize
+    unless User.find_by_id(session[:user_id])
+      redirect_to controller: 'sessions', action: 'new' , notice: "Please log in"
+    end
+  end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+    @article.user_id = session[:user_id]
+    
+      if @article.save
+    		redirect_to article_path(@article)
+  		else
+    		render 'new'
+  		end
+	end
+
+	def show
+  		@article = Article.find(params[:id])    
+  end
+
+	def index        		
+      @articles = Article.where(:user_id => session[:user_id])
+  end
+
+	def edit
+  		@article = Article.find(params[:id])
+	end
+
+	def update
+  		@article = Article.find(params[:id])
+ 
+  		if @article.update(article_params)
+    		redirect_to @article
+  		else
+    		render 'edit'
+  		end
+	end
+
+	def destroy
+  		@article = Article.find(params[:id])
+  		@article.destroy
+ 
+  		redirect_to articles_path
+	end
+
+private
+  	def article_params
+    	params.require(:article).permit(:title, :text)
+  	end
+
+end
